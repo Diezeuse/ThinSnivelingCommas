e49c43356577d77d90da8b15e31f2f74791677be
Diezeuse <115744568+Diezeuse@users.noreply.github.com>
2023-04-27 18:52:11 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let ben=  document.querySelector(".put")
+ben.addEventListener("click",function(){
+  let image = document.querySelector(".law")
+  image.src="images/goodman.gif"
+})
+
+            
+console.log(ben)
+console.log("hello")
